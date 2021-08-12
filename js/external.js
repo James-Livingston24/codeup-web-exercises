555238c2cb236e10644322c55edc8ef3c3a7a62f
James Livingston <james.b.livingston24@gmail.com>
2021-08-12 14:51:35 -0500

    complete: followed lec

---
+++ js/external.js
@@ -0,0 +1,9 @@
+"use strict";
+
+// console.log("Hello dawg!");
+// alert("You are doing fine");
+
+// var userConfirm = confirm("Do you want to remain logged in?");
+
+// alert("The user selected: " + userConfirm);
+
