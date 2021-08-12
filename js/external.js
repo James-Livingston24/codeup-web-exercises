f51d1b08f44f28ef910e21e15ac62e6e02766684
James Livingston <james.b.livingston24@gmail.com>
2021-08-12 16:31:13 -0500

    feat: completed exercises for JavaScript with HTML lesson

--- js/external.js
+++ js/external.js
@@ -6,5 +6,26 @@ var userInput = prompt("What is your favorite color?");
 
 alert("Great, " + userInput + " that is my favorite color too!");
 
-// When the exercise asks you to use a number, instead use a prompt to ask the user for that number.
-// Use an alert to show the results of each problem.
+var littleMermaid = prompt("How many days did you rent The Little Mermaid?");
+
+alert("You owe " + (littleMermaid * 3))
+
+var brotherBear = prompt("How many days did you rent Brother Bear?");
+
+alert("You owe " + (brotherBear * 3));
+
+var hercules = prompt("How many days did you rent Hercules?");
+
+alert("You owe " + (hercules * 3));
+
+var google = prompt("How much does Google pay you per hour?");
+
+alert("You will be paid " + (google * 6) + " from Google");
+
+var amazon = prompt("How much does Amazon pay you per hour?");
+
+alert("You will be paid " + (amazon * 4) + " from Amazon");
+
+var facebook  = prompt("How much does Facebook pay you per hour?");
+
+alert("You will be paid " + (facebook * 10) + " from Facebook");
