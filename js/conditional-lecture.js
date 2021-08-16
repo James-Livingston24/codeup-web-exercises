55eeda0119646c4123835cd9f7df9ccb9d6aa319
James Livingston <james.b.livingston24@gmail.com>
2021-08-16 12:30:20 -0500

    feat: conditionals lecture and exercise

---
+++ js/conditional-lecture.js
@@ -0,0 +1,50 @@
+//if statement
+
+/**var isSunny = false; //true would return below alert
+
+if(isSunny) {
+    //do some stuff
+    alert("Don't forget your sunscreen!")
+}
+
+*/
+
+// if/else statement
+/**
+var isSunny = true;
+var haveSunscreen = false;
+
+if(isSunny) {
+    alert("Don't forget some sunscreen!");
+} else {
+    alert("Great! Too much sun isn't good for your skin anyway!");
+}
+*/
+
+// if / else if / else statement
+/**
+var isSunny = confirm("Is it sunny outside today?");
+var haveSunscreen = confirm("Did you remember your sunscreen?")
+
+if(isSunny && haveSunscreen) {
+    alert("Great! You brought sunscreen, don't forget to apply!");
+}else if (isSunny && !haveSunscreen) {
+    alert("Ahh man - be careful! It's a sunny day out there!");
+} else {
+    alert("Well, hopefully it's not too bad out there!");
+}
+*/
+/**
+var trafficLight = "green";
+
+if(trafficLight === "red") {
+    alert("Stop!");
+} else if (trafficLight === "yellow") {
+    alert("Slow down!");
+} else if (trafficLight === "green") {
+    alert("Please proceed!");
+} else {
+    alert("It appears the light system is broke!");
+}
+ */
+// look at instructor's code in git to see a function use w/ if/else etc.
